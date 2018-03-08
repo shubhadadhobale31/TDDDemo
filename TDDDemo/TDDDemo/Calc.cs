850443c1187209bb953360d85d4ff0a562a69ccd
Shubhada Dhobale <678633@cognizant.com>
2018-03-08 18:15:26 +0530

    Code Added

---
+++ TDDDemo/TDDDemo/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDDemo
+{
+    public interface ICalc
+    {
+        int Add(int a, int b);
+        int Sub(int a, int b);
+    }
+    public class Calc : ICalc
+    {
+        public int Add(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return a - b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDDemo/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDDemo;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReturnAddtionWhenTwoPositiveIntsARePassed()
+        {
+            Calc c = new Calc();
+            int res = c.Add(1, 2);
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ReturnAddtionWhenOnePositiveAndOneNegativeIntsARePassed()
+        {
+            ICalc c = new Calc();
+            int res = c.Add(1, -2);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
